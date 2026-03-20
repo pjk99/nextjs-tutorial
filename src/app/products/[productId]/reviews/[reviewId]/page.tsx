15e6c3c2f9c97431dfdb43d5ea1a87102c8274ae
Preeti Kalloli <preetikalloli@Preetis-MacBook-Air.local>
2026-03-20 13:47:26 +0530

    add links and navigations

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,12 +1,13 @@
-import {notFound} from "next/navigation";
+import {notFound, redirect} from "next/navigation";
 
 export default async function ProductReview({params} : {
     params :Promise<{productId : string, reviewId : string}>
 }) {
     const {productId, reviewId} = await params;
     // either do below or create [reviewId]/not-found.tsx
-    // if(parseInt(reviewId) > 1000) {
-    //     notFound();
-    // }
+    if(parseInt(reviewId) > 1000) {
+        // notFound();
+        redirect("/products")
+    }
     return <h1>Review {reviewId} for product {productId}</h1>
 }
